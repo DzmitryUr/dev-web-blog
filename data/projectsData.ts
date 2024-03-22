306ac521154993aaef2abee18a6ea55cc56a8dbb
Dzmitry <dimaau1@gmail.com>
2024-03-22 12:05:50 +0200

    update projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -10,8 +10,15 @@ const projectsData: Project[] = [
     title: 'Dev Web Blog',
     description:
       'My Web Development Blog about programming with JavaScript, HTML5, CSS3, React, and Next.js',
-    //imgSrc: '/static/images/google.png',
+    imgSrc: '/static/images/DevWebBlog.png',
     href: 'https://www.devweb.blog/',
+  },
+  {
+    title: 'Quiz App',
+    description:
+      'Quiz App in JavaScript, HTML5, CSS3 developed and deployed during my online tutorial',
+    imgSrc: '/static/images/App-Quiz-JavaScript-HTML-CSS.png',
+    href: 'https://dzmitryur.github.io/quiz-app/',
   },
 ]
 
